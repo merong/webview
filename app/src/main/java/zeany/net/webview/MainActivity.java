fa24362836842002569eabac8eef89c2e00f710d
zeany <zeanes@gmail.com>
2017-01-05 05:54:43 +0000

    안드로이드 웹뷰(WebView)를 이용한 예제 프로그램 (1)

--- app/src/main/java/zeany/net/webview/MainActivity.java
+++ app/src/main/java/zeany/net/webview/MainActivity.java
@@ -2,12 +2,31 @@ package zeany.net.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl("http://zeany.tistory.com");
     }
 }
